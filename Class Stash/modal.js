82409e9173bab5a0688fc1bf88e4c438c58d1028
joefedota <joefedota@gmail.com>
2017-11-04 16:49:02 -0700

    removed redundancy

--- Class Stash/modal.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('#addrsrcbtn').click(function() {
-		$('#addrsrcmodal').fadeIn(400);
-	});
-	$('span#closemodal').click(function() {
-		$('#addrsrcmodal').fadeOut(400);
-	});
-});
